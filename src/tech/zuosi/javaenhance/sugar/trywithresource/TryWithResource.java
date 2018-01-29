afa60b75b65d50f3d5d7b09ab3c229bfa25b680a
LuckyKoala <pilotcraft.lk@gmail.com>
2018-01-29 13:04:34 +0800

    make use of import statement to shorten class name

--- src/tech/zuosi/javaenhance/sugar/trywithresource/TryWithResource.java
+++ src/tech/zuosi/javaenhance/sugar/trywithresource/TryWithResource.java
@@ -12,32 +12,23 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 public class TryWithResource {
-    void sugar(String zipFileName,
-                                                  String outputFileName)
-            throws java.io.IOException {
+    void sugar(String zipFileName, String outputFileName) throws java.io.IOException {
 
-        java.nio.charset.Charset charset =
-                java.nio.charset.StandardCharsets.US_ASCII;
-        java.nio.file.Path outputFilePath =
-                java.nio.file.Paths.get(outputFileName);
+        Charset charset = StandardCharsets.US_ASCII;
+        Path outputFilePath = Paths.get(outputFileName);
 
         // Open zip file and create output file with
         // try-with-resources statement
 
         try (
-                java.util.zip.ZipFile zf =
-                        new java.util.zip.ZipFile(zipFileName);
-                java.io.BufferedWriter writer =
-                        java.nio.file.Files.newBufferedWriter(outputFilePath, charset)
+                ZipFile zf = new ZipFile(zipFileName);
+                BufferedWriter writer = Files.newBufferedWriter(outputFilePath, charset)
         ) {
             // Enumerate each entry
-            for (java.util.Enumeration entries =
-                 zf.entries(); entries.hasMoreElements();) {
+            for (Enumeration entries = zf.entries(); entries.hasMoreElements();) {
                 // Get the entry name and write it to the output file
                 String newLine = System.getProperty("line.separator");
-                String zipEntryName =
-                        ((java.util.zip.ZipEntry)entries.nextElement()).getName() +
-                                newLine;
+                String zipEntryName = ((ZipEntry)entries.nextElement()).getName() + newLine;
                 writer.write(zipEntryName, 0, zipEntryName.length());
             }
         }
