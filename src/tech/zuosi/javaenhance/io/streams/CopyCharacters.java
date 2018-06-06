3f7707ff1e05a9388f8d8d52770c6fb18e42be06
LuckyKoala <pilotcraft.lk@gmail.com>
2018-06-06 21:58:31 +0800

    add lineOriented() method

--- src/tech/zuosi/javaenhance/io/streams/CopyCharacters.java
+++ src/tech/zuosi/javaenhance/io/streams/CopyCharacters.java
@@ -1,22 +1,46 @@
 package tech.zuosi.javaenhance.io.streams;
 
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class CopyCharacters {
     public static void main(String[] args) throws IOException {
+        normal();
+        lineOriented();
+    }
 
+    private static void normal() throws IOException {
         FileReader inputStream = null;
         FileWriter outputStream = null;
 
         try {
             inputStream = new FileReader("xanadu.txt");
-            outputStream = new FileWriter("outagain.txt");
+            outputStream = new FileWriter("normal.txt");
 
             int c;
             while ((c = inputStream.read()) != -1) {
                 outputStream.write(c);
+            }
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if (outputStream != null) {
+                outputStream.close();
+            }
+        }
+    }
+
+    private static void lineOriented() throws IOException {
+        BufferedReader inputStream = null;
+        PrintWriter outputStream = null;
+
+        try {
+            inputStream = new BufferedReader(new FileReader("xanadu.txt"));
+            outputStream = new PrintWriter(new FileWriter("lineOriented.txt"));
+
+            String l;
+            while ((l = inputStream.readLine()) != null) {
+                outputStream.println(l);
             }
         } finally {
             if (inputStream != null) {
